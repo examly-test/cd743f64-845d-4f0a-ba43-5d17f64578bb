2ecbc43851c5b26e4063e83c59257e0b356f123f
akashN01 <akash@ontoborn.com>
2022-10-14 09:22:54 +0000

    COMMIT-V

---
+++ index.java
@@ -0,0 +1,14 @@
+public class index {
+
+    public void Testing1() {
+    System.out.println("TESTING STARTED");
+    }
+    public void Tst2(){
+System.out.println("testing 2");
+    }
+
+public static void main(String[] args) {
+    index a = new index();
+    a.Testing1();
+    
+}}
